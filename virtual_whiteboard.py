ac04dd8144fcf8aa635da05d27bfcb98d213bec0
mohamed-amin <aboamin.mo@gmail.com>
2026-05-17 23:28:19 +0300

    Fix HandLandmarkerOptions error

--- virtual_whiteboard.py
+++ virtual_whiteboard.py
@@ -14,8 +14,7 @@ options = HandLandmarkerOptions(
     num_hands=1,
     min_hand_detection_confidence=0.5,
     min_tracking_confidence=0.5,
-    min_hand_presence_confidence=0.5,
-    model_complexity=0
+    min_hand_presence_confidence=0.5
 )
 
 cap = cv2.VideoCapture(0)
